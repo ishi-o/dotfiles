e942a96e20f9ae63a975cce9b1ebac99767867e4
ishi-o <458457289@qq.com>
2025-11-28 10:37:07 +0800

    feat：优化`lua`开发体验
    - 引入`lazydev`

--- private_dot_config/nvim/lua/config/complete/blink.lua
+++ private_dot_config/nvim/lua/config/complete/blink.lua
@@ -24,10 +24,18 @@ blink.setup({
 	},
 	sources = {
 		default = {
+			"lazydev",
 			"lsp",
 			"path",
 			"snippets",
 			"buffer",
+		},
+		providers = {
+			lazydev = {
+				name = "LazyDev",
+				module = "lazydev.integrations.blink",
+				score_offset = 100,
+			},
 		},
 	},
 	fuzzy = { implementation = "lua" },
