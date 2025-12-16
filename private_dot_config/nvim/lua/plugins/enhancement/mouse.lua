b3fa0b0b8cf7fcb7da6d43c96c393d380e870087
ishi-o <458457289@qq.com>
2025-12-16 23:55:16 +0800

    feat(mouse): 新增鼠标悬停支持
    - 引入`eagle.nvim`插件

---
+++ private_dot_config/nvim/lua/plugins/enhancement/mouse.lua
@@ -0,0 +1,11 @@
+return {
+	{
+		"soulis-1256/eagle.nvim",
+		config = function()
+			require("eagle").setup({
+				keyboard_mode = true,
+			})
+			vim.keymap.set("n", "<Tab>", ":EagleWin<CR>", { noremap = true, silent = true })
+		end,
+	},
+}
