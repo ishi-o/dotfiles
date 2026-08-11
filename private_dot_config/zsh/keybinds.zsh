cbd3c8ac6f6122da6db61908ae28adfc40e6ead1
Ishio <458457289@qq.com>
2026-08-11 16:09:42 +0800

    fix: fix fzf tab completion

--- private_dot_config/zsh/keybinds.zsh
+++ private_dot_config/zsh/keybinds.zsh
@@ -3,7 +3,7 @@
 bindkey -M viins 'jj' vi-cmd-mode
 bindkey '' autosuggest-accept
 for keymap in viins vicmd emacs; do
-    bindkey -M "$keymap" '^O' clear-screen
+	bindkey -M "$keymap" '^O' clear-screen
 done
 
 zmodload -i zsh/complist
@@ -13,3 +13,8 @@ bindkey -M menuselect '^[k' up-line-or-history
 zstyle ':fzf-tab:*' fzf-bindings 'alt-j:down' 'alt-k:up' 'ctrl-u:half-page-up' 'ctrl-d:half-page-down'
 export FZF_CTRL_R_OPTS="${FZF_CTRL_R_OPTS:+$FZF_CTRL_R_OPTS }--bind=alt-j:down,alt-k:up,ctrl-u:half-page-up,ctrl-d:half-page-down"
 export FZF_CTRL_T_OPTS="${FZF_CTRL_T_OPTS:+$FZF_CTRL_T_OPTS }--bind=alt-j:down,alt-k:up,ctrl-u:half-page-up,ctrl-d:half-page-down"
+
+if ((${+widgets[fzf-tab-complete]})); then
+	bindkey -M emacs '^I' fzf-tab-complete
+	bindkey -M viins '^I' fzf-tab-complete
+fi
